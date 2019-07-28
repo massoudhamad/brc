e09054f816c558b03129633cc9d18d8d7c2d72b0
unknown <mouhoudhoirmoudhihirdinne@gamil.com>
2019-07-28 13:52:08 +0300

    First Message

---
+++ form of island.php
@@ -0,0 +1,33 @@
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Applicant</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+    
+</head>
+<body>
+  <div id="island">
+    <h1>ISLAND FORM</h1>
+</div>
+ <div  id="container"> 
+     <form action="" method="POST">
+        <label>COMOROS'S ISLAND</label></br>
+     <select name="island" size="6">
+         <option value="NGZIDJA">NGAZIDJA</option>
+         <option value="ANJOUAN">ANJOUAN</option>
+         <option value="MOHELI">MAYOTTE</option>
+         <option value="Others"></option>
+</select></br>
+         
+         <input type="submit" id="dtn" name="sabmit">
+        <input type="cancele" id="mdr" name="cancel">
+         
+</form>
+</div>
+
+
+    
+</body>
+</html>
